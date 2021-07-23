e96485159dea53a9ba656af66e73ae118b15bf07
Prarthana-git <prarthanachaudhari@gmail.com>
2021-07-23 17:21:58 +0530

    Database Connection

---
+++ src/main/java/JavaDB.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class JavaDB {
+    public static void main(String[] args)
+    {
+        String jdbcURL="jdbc:mysql://localhost:3306/emlpoyee_payroll?useSSl=false";
+        String username="root";
+        String password="root";
+        Connection connection;
+
+        try
+        {
+            System.out.println("Connecting to database");
+            connection= DriverManager.getConnection(jdbcURL,username,password);
+            System.out.println("Connected Successfully");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+
+        } catch (SQLException | ClassNotFoundException e) {
+                 e.printStackTrace();
+        }
+        listDrivers();
+    }
+    private static void listDrivers()
+    {
+        Enumeration<Driver> driverList =DriverManager.getDrivers();
+        while(driverList.hasMoreElements()){
+            Driver driverClass=(Driver) driverList.nextElement();
+            System.out.println(""+driverClass.getClass().getName());
+        }
+    }
+}
